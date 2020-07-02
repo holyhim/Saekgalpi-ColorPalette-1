e6dd24cf0405e22b15bccc7ffaca8c207f42eb7e
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-07-02 17:21:05 +0900

    :art: Update PaletteDetail.js
    - 팔레트 디테일 페이지의 기본 뼈대를 잡았습니다.

--- client/src/pages/PaletteDetail.js
+++ client/src/pages/PaletteDetail.js
@@ -2,10 +2,44 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const PaletteDetail = () => {
+    //TODO: 이 페이지 들어올 때 해당 팔레트 GET 요청
+    //props를 사용하여 변경
+
     return (
         <main>
-            <h1>팔레트 디테일 </h1>
-            <Link to='/editPalette/:id'>색갈피 편집</Link>
+            <div>
+                {/* className: wrapper */}
+                <h1> 팔레트 디테일 </h1>
+                <div>
+                    {/* 내부 감싸기 위한 div */}
+                    <div>
+                        {/* left palette */}
+                        <div>팔레트 props </div>
+                        <Link to='/editPalette/:id'>색갈피 편집</Link>
+                        <div>
+                            {/* 팔레트 쉐어 */}
+                            <ul>
+                                <li>
+                                    <button>공유</button>
+                                </li>
+                                <li>
+                                    <button>다운로드</button>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div>
+                        {/* right desc */}
+                        <div>
+                            <span>팔레트 제목</span>
+                            <span>닉네임</span>
+                        </div>
+                        <article>hex</article>
+                        <article>rgb</article>
+                    </div>
+                </div>
+                <div> 설명 칸 </div>
+            </div>
         </main>
     );
 };
