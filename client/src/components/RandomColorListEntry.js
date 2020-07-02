6a972c1918b175f05d5289c30d4fe4a2515ed2e6
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-07-02 20:58:20 +0900

    :bulb: Add comment
    - 해당 파일에 필요한 주석을 작성하였습니다.

--- client/src/components/RandomColorListEntry.js
+++ client/src/components/RandomColorListEntry.js
@@ -3,20 +3,9 @@ import React from 'react';
 const RandomColorListEntry = ({ color }) => {
     // TODO: 마우스 컬러 div에 마우스가 올라갔는지를 판별하는 state 작성
 
-    const onMouseEnterColor = () => {
-        // TODO: 마우스 컬러 div에 마우스 올렸을 때 컬러 정보 div 나오도록 제어 -> state값 true
-    };
-
-    const onMouseLeaveColor = () => {
-        // TODO: 마우스 컬러 div에서 마우스 나갔을 때 컬러 정보 div 숨김 -> state값 false
-    };
-
     return (
         <div>
-            <div
-                onMouseEnter={onMouseEnterColor}
-                onMouseLeave={onMouseLeaveColor}
-            >
+            <div>
                 {color}
                 {/* background-color를 내려받은 color로 */}
             </div>
