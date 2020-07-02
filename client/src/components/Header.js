1ee203d2a9345f0df1d399cf5d69cb7096890b7e
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-07-02 13:25:36 +0900

    :art: Update Header Remark
    - 헤더 파일에 필요한 주석과 불필요한 공백을 정리했습니다.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,11 +3,10 @@ import { useLocation } from 'react-router-dom';
 import MainHeader from './MainHeader';
 import SubHeader from './SubHeader';
 
+//메인과 서브 헤더를 바꾸는 구간입니다.
 const Header = () => {
     let location = useLocation();
-
     const [path, setPath] = useState(location.pathname);
-
     const changeLocation = (path) => {
         setPath(path);
     };
