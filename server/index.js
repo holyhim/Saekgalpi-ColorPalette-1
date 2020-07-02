211e7dd668262951fdfb61879fa3a10712a5a276
Heo-JiSeong <heojiseong0811@gmail.com>
2020-07-02 17:25:34 +0900

    :wrench: Updated index.js
    - 라우팅(user, color) 추가하였습니다.

--- server/index.js
+++ server/index.js
@@ -5,20 +5,8 @@ const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
-const {
-  colorEdit,
-  colorDrop,
-  colorInfo,
-  visitCheck,
-} = require('/controller/colorController');
-
-const {
-  signIn,
-  signUp,
-  signOut,
-  userInfo,
-  admin,
-} = require('/controller/userController');
+const userRouter = require('./routes/user');
+const colorRouter = require('./routes/color');
 
 const app = express();
 const port = 5000;
@@ -49,15 +37,9 @@ app.get('/', (req, res) => {
   res.status(200).send('Success');
 });
 
-app.get('/MyPage', userInfo);
-app.post('/SignIn', signIn);
-app.post('/SignUp', signUp);
-app.post('/', signOut);
-app.post('/MyPage', admin);
+app.use('/user', userRouter);
+app.use('/color', colorRouter);
 
-app.get('/PaletteDetail', colorInfo);
-app.post('/MyPage', colorDrop);
-app.post('/MakePalette', colorEdit);
-app.post('/PaletteDetail', visitCheck);
+// 40, 41번 line에 대한 것은 Check가 필요합니다.
 
 module.exports = app;
