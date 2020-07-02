5514a29afb5f92776374d2d3b20736095289d823
lsy26499 <lsy26499@gmail.com>
2020-07-02 14:29:59 +0900

    :art: Update SignUp.js
    - SignUp.js에 HTML 태그 구성과 이벤트 리스너 구조를 추가했습니다.

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,7 +1,62 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
-const SignUp = () => {
-    return <main> 가입 </main>;
+const SignUp = ({ history }) => {
+    const onClickSignUpButton = (e) => {
+        e.preventDefault();
+        // TODO: 서버로 회원가입 POST 요청 (axios 사용)
+
+        history.push('/');
+    };
+
+    const handleInputValue = (e) => {
+        // TODO: Input 값 value로 받아 state 설정
+        // e.target.value
+    };
+
+    return (
+        <main>
+            <h1> 회원가입 </h1>
+            <div>
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                    }}
+                >
+                    <div>
+                        <input
+                            type='text'
+                            placeholder='닉네임'
+                            name='username'
+                            onChange={handleInputValue}
+                        />
+                        {/* // TODO : 프로필 -> 컬러피커 패키지: 피커 모양은 간단한걸로 */}
+                        <input
+                            type='text'
+                            placeholder='이메일'
+                            name='email'
+                            onChange={handleInputValue}
+                        />
+                        <input
+                            type='text'
+                            placeholder='비밀번호'
+                            name='password'
+                            onChange={handleInputValue}
+                        />
+                        <input
+                            type='text'
+                            placeholder='비밀번호를 다시 한 번 입력하세요'
+                            name='passwordCheck'
+                            onChange={handleInputValue}
+                        />
+                    </div>
+                    <div>
+                        <button onClick={onClickSignUpButton}>회원가입</button>
+                    </div>
+                </form>
+            </div>
+        </main>
+    );
 };
 
-export default SignUp;
+export default withRouter(SignUp);
