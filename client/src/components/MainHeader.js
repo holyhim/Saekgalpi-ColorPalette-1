0f9e83f1490f6facf002c16512b6cd4f3ead651c
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-07-02 14:09:50 +0900

    :art: Update components
    - 컴포넌트 폴더 안에 있는 메인 헤더와 서브 헤더, 푸터의 html 뼈대를 작업했습니다.

--- client/src/components/MainHeader.js
+++ client/src/components/MainHeader.js
@@ -2,29 +2,36 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const MainHeader = () => {
+    //주석 나중에 제거하거나 다듬을 것
     return (
-        <>
-            <ul>
-                <li>
-                    <Link to='/signIn'>로그인</Link>
-                </li>
-                <li>
-                    <Link to='/signUp'>회원가입</Link>
-                </li>
-                <li>
-                    <Link to='/MyPage'>내 색갈피</Link>
-                </li>
-                <li>
-                    <button>로그아웃</button>
-                </li>
-            </ul>
+        <div>
+            {/*wrapper 전체를 감싸는 부분*/}
+            <nav>
+                {/*내비게이터*/}
+                <ul>
+                    <li>
+                        <Link to='/signIn'>로그인</Link>
+                    </li>
+                    <li>
+                        <Link to='/signUp'>회원가입</Link>
+                    </li>
+                    <li>
+                        <Link to='/MyPage'>내 색갈피</Link>
+                    </li>
+                    <li>
+                        <button>로그아웃</button>
+                    </li>
+                </ul>
+            </nav>
+
             <div>
+                {/*수채화 백그라운드 넣어 봅시다*/}
                 <Link to='/'>로고: MainHeader 입니다</Link>
             </div>
             <div>
                 <Link to='/allPalette'>모든 색갈피 보기</Link>
             </div>
-        </>
+        </div>
     );
 };
 
