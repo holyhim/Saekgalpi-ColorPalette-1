56939a4442358319b58ffa9d3ada980cb52cd1e1
lsy26499 <lsy26499@gmail.com>
2020-07-02 17:25:50 +0900

    :construction: Update Main.js
    - Main.js의 구조를 작성하였고, 추후 컴포넌트를 조립할 예정입니다.

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import RandomColorList from '../components/RandomColorList';
 
 const Main = () => {
+    // TODO: 메인 페이지 들어오자마자 팔레트 정보 불러오기 GET 요청 (useEffect, axios)
+
     return (
         <main>
             <h1>메인</h1>
-            <Link to='/makePalette'>내 색갈피 만들기</Link>
+            <button>
+                <Link to='/makePalette'>내 색갈피 만들기</Link>
+            </button>
+            <section>
+                추후 인기순 팔레트 정렬 컴포넌트로 대체될 예정입니다
+            </section>
+            <section>
+                추후 최신순 팔레트 정렬 컴포넌트로 대체될 예정입니다
+            </section>
+            <RandomColorList />
         </main>
     );
 };
