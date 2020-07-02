492196edbf57a8e67cefadd58accb6af50837b0e
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-07-02 20:55:05 +0900

    :bulb: Add comment
    팔레트 수정 리스트 페이지에 필요한 주석을 넣었습니다.

--- client/src/components/EditPaletteHexList.js
+++ client/src/components/EditPaletteHexList.js
@@ -3,6 +3,7 @@ import React from 'react';
 const EditPaletteHexList = () => {
     //주석 나중에 제거하거나 다듬을 것
     //컬러 api 사용
+    //숫자에 맞게 div를 생성하는 헬퍼 함수를 만들어야 합니다
     const createDIVbox = (range) => {
         //갯수에 맞게 div 갯수 만들거나 줄임
     };
