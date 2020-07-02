11f1c408b0d37749ae6282667c374a0e9adab262
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-07-02 20:59:52 +0900

    :art: Update a Palette list in page
    - 페이지 안에 들어갈 팔레트 리스트를 넣었습니다.

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import RandomColorList from '../components/RandomColorList';
+import PaletteList from '../components/PaletteList';
 
 const Main = () => {
     // TODO: 메인 페이지 들어오자마자 팔레트 정보 불러오기 GET 요청 (useEffect, axios)
@@ -12,10 +13,10 @@ const Main = () => {
                 <Link to='/makePalette'>내 색갈피 만들기</Link>
             </button>
             <section>
-                추후 인기순 팔레트 정렬 컴포넌트로 대체될 예정입니다
+                <PaletteList />
             </section>
             <section>
-                추후 최신순 팔레트 정렬 컴포넌트로 대체될 예정입니다
+                <PaletteList />
             </section>
             <RandomColorList />
         </main>
