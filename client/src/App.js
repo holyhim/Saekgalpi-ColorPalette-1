7951568f62981b69a7d15b625595bce82a2b7207
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-07-03 14:39:20 +0900

    :lipstick: Add style files and test
    - css에 관한 파일을 설치했고 기능에 대해 가볍게 테스트했습니다.

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import Router from './Router';
 import Footer from './components/templete/Footer';
-import './App.css';
+import './App.scss';
 
 const App = () => {
     return (
