049b1a840d211623b77e3ebb6e6a69298c9ce1bc
Heo-JiSeong <heojiseong0811@gmail.com>
2020-07-03 14:16:25 +0900

    :sparkles: Created signIn API
    - signIn API 작성하였습니다.

---
+++ server/controller/userController/signIn.js
@@ -0,0 +1,24 @@
+const { user } = require('./models');
+
+module.exports = {
+  signIn: (req, res) => {
+    const { email, password } = req.body;
+    user
+      .findOne({
+        where: {
+          email: email,
+          password: password,
+        },
+      })
+      .then((result) => {
+        if (!result.body.email) {
+          res.status(404).send('Email Not found');
+          alert('이메일을 확인해주세요.');
+        } else if (!result.body.password) {
+          res.status(404).send('Password Wrong');
+          alert('비밀번호를 확인해주세요.');
+        } else res.status(200).send('Signin Sucsses');
+        alert(`${req.body.email}님 어서오세요 !`);
+      });
+  },
+};
