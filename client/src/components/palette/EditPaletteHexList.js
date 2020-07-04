0319fcdadec20b8d15a261c51e79acf0813c3725
lsy26499 <lsy26499@gmail.com>
2020-07-04 15:26:53 +0900

    :lipstick: Update palette components styles
    - 팔레트 컴포넌트들의 스타일을 변경했습니다.

--- client/src/components/palette/EditPaletteHexList.js
+++ client/src/components/palette/EditPaletteHexList.js
@@ -1,4 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const HexLists = styled.div`
+    display: grid;
+    grid-template-columns: repeat(${(props) => props.number}, 1fr);
+    grid-template-rows: 200px;
+`;
+// TODO: 추후 데이터받아서 props.number로 넘겨주기
+
+const PaletteHex = styled.div`
+    width: 500px
+    height: 200px;
+    border: 1px solid black;
+`;
 
 const EditPaletteHexList = () => {
     //주석 나중에 제거하거나 다듬을 것
@@ -8,11 +22,15 @@ const EditPaletteHexList = () => {
         //갯수에 맞게 div 갯수 만들거나 줄임
     };
     return (
-        <div onChange={createDIVbox}>
+        <HexLists
+            className='edit-palette__hex-lists'
+            onChange={createDIVbox}
+            number={2}
+        >
             {/*2개부터 7개까지 div*/}
-            <div>hex, rgb</div>
-            <div>hex, rgb</div>
-        </div>
+            <PaletteHex className='edit-palette__hex'>hex, rgb</PaletteHex>
+            <PaletteHex className='edit-palette__hex'>hex, rgb</PaletteHex>
+        </HexLists>
     );
 };
 
--- client/src/components/palette/RandomColorList.js
+++ client/src/components/palette/RandomColorList.js
@@ -1,28 +1,36 @@
 // 랜덤컬러 전체 그리드
 import React from 'react';
 import RandomColorListEntry from './RandomColorListEntry';
+import styled from 'styled-components';
+
+const RandomColorsContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    gap: 10px;
+    height: 40vh;
+`;
 
 const RandomColorList = () => {
     // TODO: Main페이지 렌더되었을 때 랜덤컬러 7개 생성하는 헬퍼함수 제작
     const fakeColor = [
-        '가짜',
-        '컬러',
-        '데이터',
-        '입니다',
-        '추후',
-        '수정',
-        '예정',
+        '#FFC312',
+        '#F79F1F',
+        '#C4E538',
+        '#0652DD',
+        '#1B1464',
+        '#9980FA',
+        '#B53471',
     ];
 
     return (
-        <section>
-            <h3>랜덤컬러 생성기</h3>
-            <div>
+        <section className='random-colors__wrapper'>
+            <h3 className='random-colors__title'>랜덤컬러 생성기</h3>
+            <RandomColorsContainer>
                 {fakeColor.map((color, idx) => (
                     <RandomColorListEntry key={idx} color={color} />
                     // 추후 키값은 상의해서 변경 예정
                 ))}
-            </div>
+            </RandomColorsContainer>
         </section>
     );
 };
