8202825e886523587c4206b6ddc4136019756540
Heo-JiSeong <heojiseong0811@gmail.com>
2020-07-05 16:27:07 +0900

    :wrench: Updated signIn API
    - signIn API를 수정하였습니다.

--- server/controller/userController/signIn.js
+++ server/controller/userController/signIn.js
@@ -3,22 +3,21 @@ const { User } = require('../../models');
 module.exports = {
   post: (req, res) => {
     const { email, password } = req.body;
-    User
-      .findOne({
-        where: {
-          email: email,
-          password: password,
-        },
-      })
+    User.findOne({
+      where: {
+        email: email,
+        password: password,
+      },
+    })
       .then((result) => {
-        if (!result.body.email) {
-          res.status(404).send('Email Not found');
-          alert('이메일을 확인해주세요.');
-        } else if (!result.body.password) {
-          res.status(404).send('Password Wrong');
-          alert('비밀번호를 확인해주세요.');
-        } else res.status(200).send('Signin Sucsses');
-        alert(`${req.body.email}님 어서오세요 !`);
+        if (!email) {
+          res.status(404).send('Email is Null');
+          //alert('이메일을 입력해주세요.');
+        } else if (!password) {
+          res.status(404).send('Password is Null');
+          //alert('비밀번를 입력해주세요.');
+        } else res.status(200).send(result);
+        //alert(`${result.email}님 어서오세요 !`);
       })
       .catch((err) => {
         if (err) {
