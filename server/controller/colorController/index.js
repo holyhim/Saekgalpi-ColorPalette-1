5470f89b80643939c17ef6221c4f3a4abd0c6682
Trisha Park <61039975+Trisha-Park@users.noreply.github.com>
2020-07-06 18:45:19 +0900

    :fire: Changed FileName
    color -> palette

--- server/controller/colorController/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  colorCreate: require('./colorCreate'),
-  colorDrop: require('./colorDrop'),
-  colorEdit: require('./colorEdit'),
-  colorInfo: require('./colorInfo'),
-  visitCheck: require('./visitCheck'),
-};
