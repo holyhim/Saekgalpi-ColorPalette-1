c5084241ca509b3fa3eb0eac19ed8e83f84fc1b5
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-07-06 12:09:08 +0900

    :lipstick: Update Pages css
    - 페이지의 css를 업데이트하였습니다.

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -2,15 +2,36 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PaletteList from '../components/palette/PaletteList';
 import RandomColorList from '../components/palette/RandomColorList';
+import './Main.scss';
+import { Button } from 'antd';
+import styd from 'styled-components';
+
+const SubHeaderButton = styd(Button)`
+color: #a6a6a6;
+background-color: #e7e7e7;
+border: 1px solid #e7e7e7;
+margin: 10px;
+margin-bottom: 120px;
+width: 840px;
+height: 100px;
+font-size: 3em;
+transition: all .3s;
+&:hover{
+    background-color: #e7e7e7;
+    border: 1px solid #e7e7e7;
+    color: #5d5d5d;
+}
+
+`;
 
 const Main = () => {
     // TODO: 메인 페이지 들어오자마자 팔레트 정보 불러오기 GET 요청 (useEffect, axios)
 
     return (
         <main className='main__main-content'>
-            <button className='make-palette__link-btn'>
+            <SubHeaderButton>
                 <Link to='/makePalette'>내 색갈피 만들기</Link>
-            </button>
+            </SubHeaderButton>
             <section className='fav-palettes__container'>
                 <h3 className='fav-palettes__title'>인기순 색갈피</h3>
                 <PaletteList />
