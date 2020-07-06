fc475bbd1b883a8fd2cd68513d01b8d03d1d62f4
Trisha Park <61039975+Trisha-Park@users.noreply.github.com>
2020-07-06 23:33:53 +0900

    :wrench: Modified paletteCreate
    - 팔레트 생성 API를 수정하였습니다. - User에서 아이디 확인 후 생성합니다..

--- server/controller/paletteController/paletteCreate.js
+++ server/controller/paletteController/paletteCreate.js
@@ -1,4 +1,4 @@
-const { Palette } = require('../../models');
+const { Palette, User } = require('../../models');
 
 module.exports = {
     post: (req, res) => {
@@ -13,25 +13,30 @@ module.exports = {
             colorCode06,
             colorCode07,
         } = req.body;
-        Palette.findOrCreate({
+        const { id } = req.params;
+        User.findOne({
             where: {
-                userId,
-            },
-            defaults: {
-                paletteName,
-                description,
-                colorCode01,
-                colorCode02,
-                colorCode03,
-                colorCode04,
-                colorCode05,
-                colorCode06,
-                colorCode07,
+                id,
             },
         })
             .then((data) => {
                 if (data) {
-                    res.status(200).send(data);
+                    Palette.create({
+                        paletteName,
+                        description,
+                        colorCode01,
+                        colorCode02,
+                        colorCode03,
+                        colorCode04,
+                        colorCode05,
+                        colorCode06,
+                        colorCode07,
+                    });
+                }
+            })
+            .then((result) => {
+                if (result) {
+                    res.status(200).send(result);
                 } else {
                     res.status(404).send('Bad Request');
                     alert('잘못 된 요청입니다');
