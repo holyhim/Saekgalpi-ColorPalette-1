4aaf611479e9452f91e65ff4bf80298a03bd2af9
lsy26499 <lsy26499@gmail.com>
2020-07-06 12:00:37 +0900

    :bug: Fix bugs of PaletteListEntry
    - 낱개 팔레트에 hover했을 때 팔레트 정보가 제대로 표시되지 않는 오류를 수정했습니다.

--- client/src/components/palette/PaletteListEntry.js
+++ client/src/components/palette/PaletteListEntry.js
@@ -1,13 +1,15 @@
 // 낱개 팔레트 회원과 해당 팔레의 userid가 같다면 딜리트 버튼이 있어야 함
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import { withRouter } from 'react-router-dom';
+import domtoimage from 'dom-to-image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faHeart,
+    faArrowDown,
+    faShareAlt,
+} from '@fortawesome/free-solid-svg-icons';
 import styled from 'styled-components';
-
-const PaletteWrapper = styled.div`
-    position: relative;
-    width: 200px;
-    height: 200px;
-`;
+import './PaletteListEntry.scss';
 
 const PaletteColors = styled.div`
     position: absolute;
@@ -15,19 +17,15 @@ const PaletteColors = styled.div`
     grid-template-columns: repeat(${(props) => props.number}, 1fr);
     width: 200px;
     height: 200px;
+    cursor: pointer;
+    // .palette__color:first-child {
+    //     border-radius: 8px 0 0 8px;
+    // }
+    // .palette__color:last-child {
+    //     border-radius: 0 8px 8px 0;
+    // }
 `;
 // TODO: 추후 데이터받아서 props.number로 넘겨주기
-
-const PaletteInfo = styled.div`
-    position: absolute;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    width: 200px;
-    background-color: #f1f2f6;
-    padding: 5px 0;
-    bottom: 0px;
-`;
 
 const PaletteColor = styled.div`
     background-color: ${(props) => props.color};
@@ -36,46 +34,61 @@ const PaletteColor = styled.div`
 const PaletteListEntry = ({ history }) => {
     //주석 나중에 제거하거나 다듬을 것
     //숫자에 맞게 div를 생성하는 헬퍼 함수를 만들어야 합니다 -> 받은 숫자에 맞게 map으로 div를 만들어주는 건 어떨까요?
-    const [isMouseOver, setIsMouseOver] = useState(false);
-    const [colors, setColors] = useState(['red', 'blue', 'black']);
+    const fakeColors = ['#74b9ff', '#0984e3', '#70a1ff'];
+    const palette = useRef(null);
 
-    const toggleInfo = () => {
-        setIsMouseOver((prevState) => !prevState);
-    };
-
-    const onClickPalette = () => {
+    const onClickPalette = (e) => {
         history.push('/paletteDetail/:id');
     };
 
+    const onClickDownload = async () => {
+        try {
+            const dataUrl = await domtoimage.toPng(palette.current);
+            const link = document.createElement('a');
+            link.href = dataUrl;
+            link.download = '색갈피';
+            link.click();
+        } catch (error) {
+            console.error('oops, something went wrong!', error);
+        }
+    };
+
+    //* map key 수정
     return (
-        <PaletteWrapper className='palette__wrapper' onClick={onClickPalette}>
-            {/* wrapper */}
+        <div className='palette__wrapper'>
             <PaletteColors
                 className='palette__colors'
-                onMouseOver={toggleInfo}
-                onMouseOut={toggleInfo}
-                number={colors.length}
+                number={fakeColors.length}
+                onClick={onClickPalette}
+                ref={palette}
             >
-                {/* div를 묶어 줘야 됨 */}
-                {colors.map((color) => (
-                    <PaletteColor className='palette__color' color={color} />
+                {fakeColors.map((color, idx) => (
+                    <PaletteColor
+                        className='palette__color'
+                        color={color}
+                        key={idx}
+                    />
                 ))}
             </PaletteColors>
-            {isMouseOver ? (
-                <PaletteInfo className='palette__info--hidden'>
-                    {/* hover page */}
-                    <span className='palette__title'>타이틀</span>
-                    <div className='palette__icons'>
-                        <button className='palette__like'>ㅁ</button>
-                        <button className='palette__save'>저장</button>
-                        <button className='palette__share'>공유</button>
-                    </div>
-                </PaletteInfo>
-            ) : (
-                <></>
-            )}
+            <div className='palette__info--hidden'>
+                <span className='palette__title'>타이틀</span>
+                <div className='palette__icons'>
+                    <button className='palette__like'>
+                        <FontAwesomeIcon icon={faHeart} />
+                    </button>
+                    <button className='palette__save'>
+                        <FontAwesomeIcon
+                            icon={faArrowDown}
+                            onClick={onClickDownload}
+                        />
+                    </button>
+                    <button className='palette__share'>
+                        <FontAwesomeIcon icon={faShareAlt} />
+                    </button>
+                </div>
+            </div>
             <button className='palette__delete--hidden'>삭제</button>
-        </PaletteWrapper>
+        </div>
     );
 };
 
