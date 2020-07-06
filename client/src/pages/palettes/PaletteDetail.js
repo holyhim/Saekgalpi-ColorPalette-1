abcc8490acb3c91ae985baf8b8d97a43dff623db
lsy26499 <lsy26499@gmail.com>
2020-07-06 14:49:50 +0900

    :lipstick: Update styles of palette pages
    - 팔레트 페이지들의 css 스타일을 업데이트했습니다.

--- client/src/pages/palettes/PaletteDetail.js
+++ client/src/pages/palettes/PaletteDetail.js
@@ -1,16 +1,69 @@
 import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { Button } from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowDown, faShareAlt } from '@fortawesome/free-solid-svg-icons';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import domtoimage from 'dom-to-image';
 import './PaletteDetail.scss';
 
-// TODO: 클릭시 카피되는것
 const Paltte = styled.div`
     background-color: ${(props) => props.color || 'black'};
     cursor: pointer;
+`;
+
+const PaletteWrapper = styled.div`
+    width: 30vw;
+    height: 30vh;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+`;
+
+const EditButton = styled(Button)`
+    color: #a6a6a6;
+    background-color: #e7e7e7;
+    border: 1px solid #e7e7e7;
+    margin-top: 30px;
+
+    &:hover {
+        background-color: #e7e7e7;
+        border: 1px solid #e7e7e7;
+        color: #5d5d5d;
+    }
+    &:focus {
+        background-color: #e7e7e7;
+        border: 1px solid #e7e7e7;
+        color: #a6a6a6;
+    }
+    &:after {
+        --antd-wave-shadow-color: #c7c7c7;
+    }
+`;
+
+const ShareBtn = styled.button`
+    all: unset;
+    font-size: 1.5rem;
+    cursor: pointer;
+    margin-right: 1rem;
+`;
+
+const ColorCodeContainer = styled.article``;
+
+const ColorCodeDetail = styled.div`
+    display: grid;
+    grid-template-columns: repeat(${(props) => props.num}, 1fr);
+    line-height: 6vh;
+`;
+
+const ColorCode = styled.div`
+    background-color: ${(props) => props.color};
+    text-align: center;
+`;
+
+const PaletteDescription = styled.div`
+    margin-left: 4vw;
+    height: 5vw;
 `;
 
 const PaletteDetail = () => {
@@ -33,49 +86,82 @@ const PaletteDetail = () => {
 
     return (
         <main className='palette-detail__main'>
-            <h1 className='palette-detail__title'>색갈피 상세</h1>
-            <div className='palette-detail__container'>
-                <div className='palette-detail__column'>
-                    <div className='palette-detail__palette' ref={palette}>
-                        {fakeColors.map((color, idx) => (
-                            <CopyToClipboard text={color} key={idx}>
-                                <Paltte color={color} key={idx} />
-                            </CopyToClipboard>
-                        ))}
+            <h1 className='palette-detail__title h1'>색갈피 상세</h1>
+            <div className='palette-detail__all-wrapper'>
+                <div className='palette-detail__container'>
+                    <div className='palette-detail__column'>
+                        <PaletteWrapper
+                            className='palette-detail__palette'
+                            ref={palette}
+                        >
+                            {fakeColors.map((color, idx) => (
+                                <CopyToClipboard text={color} key={idx}>
+                                    <Paltte color={color} key={idx} />
+                                </CopyToClipboard>
+                            ))}
+                        </PaletteWrapper>
+                        <div className='palette-detail__share-container'>
+                            <EditButton className='palette-detail__edit-btn'>
+                                <Link to='/editPalette/:id'>색갈피 편집</Link>
+                            </EditButton>
+                            <ul className='palette-detail__share-lists'>
+                                <li className='palette-detail__share-item'>
+                                    <ShareBtn className='palette-detail__item-link'>
+                                        <FontAwesomeIcon icon={faShareAlt} />
+                                    </ShareBtn>
+                                </li>
+                                <li className='palette-detail__share-item'>
+                                    <ShareBtn
+                                        className='palette-detail__item-download'
+                                        onClick={onClickDownload}
+                                    >
+                                        <FontAwesomeIcon icon={faArrowDown} />
+                                    </ShareBtn>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                    <div className='palette-detail__share-container'>
-                        <button className='palette-detail__edit-btn'>
-                            <Link to='/editPalette/:id'>색갈피 편집</Link>
-                        </button>
-                        <ul className='palette-detail__share-lists'>
-                            <li className='palette-detail__share-item'>
-                                <button className='palette-detail__item-link'>
-                                    <FontAwesomeIcon icon={faShareAlt} />
-                                </button>
-                            </li>
-                            <li className='palette-detail__share-item'>
-                                <button
-                                    className='palette-detail__item-download'
-                                    onClick={onClickDownload}
-                                >
-                                    <FontAwesomeIcon icon={faArrowDown} />
-                                </button>
-                            </li>
-                        </ul>
+                    <div className='palette-detail__column'>
+                        <div className='palette-detail__info'>
+                            <span className='palette-detail__title'>
+                                팔레트 제목
+                            </span>
+                            <span className='palette-detail__username'>
+                                닉네임
+                            </span>
+                        </div>
+                        <ColorCodeContainer className='palette-detail__code-container'>
+                            <h5>HEX</h5>
+                            <ColorCodeDetail
+                                className='palette-detail__hex'
+                                num={fakeColors.length}
+                            >
+                                {fakeColors.map((color, idx) => (
+                                    <ColorCode color={color} key={idx}>
+                                        {color}
+                                    </ColorCode>
+                                ))}
+                            </ColorCodeDetail>
+                        </ColorCodeContainer>
+                        <ColorCodeContainer className='palette-detail__code-container'>
+                            <h5>RGB</h5>
+                            <ColorCodeDetail
+                                className='palette-detail__rgb'
+                                num={fakeColors.length}
+                            >
+                                {fakeColors.map((color, idx) => (
+                                    <ColorCode color={color} key={idx}>
+                                        rgb
+                                    </ColorCode>
+                                ))}
+                            </ColorCodeDetail>
+                        </ColorCodeContainer>
                     </div>
                 </div>
-                <div className='palette-detail__column'>
-                    <div className='palette-detail__info'>
-                        <span className='palette-detail__title'>
-                            팔레트 제목
-                        </span>
-                        <span className='palette-detail__username'>닉네임</span>
-                    </div>
-                    <article className='palette-detail__hex'>hex</article>
-                    <article className='palette-detail__rgb'>rgb</article>
-                </div>
+                <PaletteDescription className='palette-detail__description'>
+                    설명 칸
+                </PaletteDescription>
             </div>
-            <div className='palette-detail__description'> 설명 칸 </div>
         </main>
     );
 };
