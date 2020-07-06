4aae76c736c915e76ec997810a46117b478a4373
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-07-06 17:00:40 +0900

    :lipstick: Update global scss
    - 전역 scss를 수정했습니다.

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import Router from './Router';
-import Footer from './components/templete/Footer';
-import './App.scss';
+import Footer from './components/templete/Footer.js';
+
+import './scss/config/App.scss';
 import 'antd/dist/antd.css';
 
 const App = () => {
