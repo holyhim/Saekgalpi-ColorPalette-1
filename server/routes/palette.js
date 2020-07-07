1fa362a2220f35e1d2788ce15383400b717b36a4
Heo-JiSeong <heojiseong0811@gmail.com>
2020-07-07 16:10:02 +0900

    :wrench: Updated routes/palette.js
    - visitCheck route 주석 해제하였습니다. - route url에 대한 설명 수정하였습니다.

--- server/routes/palette.js
+++ server/routes/palette.js
@@ -3,25 +3,25 @@ const router = express.Router();
 
 const paletteController = require('../controller/paletteController');
 
-// * POST /colorController/colorCreate
+// * POST /colorController/paletteCreate
 router.post('/makePalette', paletteController.paletteCreate.post);
 
-// * POST /colorController/colorDrop
+// * POST /colorController/paletteDrop
 router.post(
   ['/MyPage', '/admin', 'paletteDetail/:id'],
   paletteController.paletteDrop.post
 );
 
-// * POST /colorController/colorEdit
+// * POST /colorController/paletteEdit
 router.post('/editPalette/:id', paletteController.paletteEdit.post);
 
-// * GET /colorController/colorInfo
+// * GET /colorController/paletteInfo
 // router.get(
 //   ['/', '/paletteDetail/:id', '/allPalette', '/editPalette/:id'],
 //   paletteController.paletteInfo.get
 // );
 
 // * GET /colorController/visitCheck
-//router.get(['/', '/paletteDetail/:id'], paletteController.visitCheck.get);
+router.get('/paletteDetail/:id', paletteController.visitCheck.get);
 
 module.exports = router;
