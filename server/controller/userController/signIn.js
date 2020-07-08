cabad184a311b17b1ebd13cec44a9689d9570924
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-07-08 22:14:12 +0900

    :pencil2: Fix typos
    - 오타 수정 / 저장 버튼 눌러서 생긴 수정 사항입니다.

--- server/controller/userController/signIn.js
+++ server/controller/userController/signIn.js
@@ -1,30 +1,30 @@
 const { User } = require('../../models');
 
 module.exports = {
-  post: (req, res) => {
-    const { email, password } = req.body;
-    let session = req.session;
-    User.findOne({
-      where: {
-        email: email,
-        password: password,
-      },
-    })
-      .then((data) => {
-        if (!email) {
-          res.status(401).send('Email is Null');
-          alert('이메일을 입력해주세요.');
-        } else if (!password) {
-          res.status(401).send('Password is Null');
-          alert('비밀번를 입력해주세요.');
-        } else {
-          session.email = email;
-          res.status(200).send(data);
-          alert(`${result.email}님 어서오세요 !`);
-        }
-      })
-      .catch((err) => {
-        res.status(500).send(err);
-      });
-  },
+    post: (req, res) => {
+        const { email, password } = req.body;
+        let session = req.session;
+        User.findOne({
+            where: {
+                email: email,
+                password: password,
+            },
+        })
+            .then((data) => {
+                if (!email) {
+                    res.status(401).send('Email is Null');
+                    //alert('이메일을 입력해주세요.');
+                } else if (!password) {
+                    res.status(401).send('Password is Null');
+                    //alert('비밀번를 입력해주세요.');
+                } else {
+                    session.email = email;
+                    res.status(200).send(data);
+                    //alert(`${result.email}님 어서오세요 !`);
+                }
+            })
+            .catch((err) => {
+                res.status(500).send(err);
+            });
+    },
 };
