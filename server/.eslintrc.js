2ef3c02d98229c8db7a82798f85ace464350ac68
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-07-09 17:29:43 +0900

    :bulb: Modify Eslintrc
    - eslint를 수정하였습니다.

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2020: true,
-  },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        commonjs: true,
+        es2020: true,
     },
-    ecmaVersion: 11,
-  },
-  plugins: ['react'],
-  rules: {
-    'no-console': 'off',
-    'no-unused-vars': [
-      'error',
-      { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
-    ],
-  },
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 11,
+    },
+    plugins: ['react'],
+    rules: {
+        'no-console': 'off',
+        'no-unused-vars': [
+            'error',
+            { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
+        ],
+    },
 };
