ef24705015aa335d61a1c8d49f045257bbc1e1b0
Heo-JiSeong <heojiseong0811@gmail.com>
2020-07-09 16:05:52 +0900

    :wrench: Updated signUp API
    - signaturecolor 기본 값 설정해주었습니다.

--- server/controller/userController/signUp.js
+++ server/controller/userController/signUp.js
@@ -1,50 +1,38 @@
 const { User } = require('../../models');
 
 module.exports = {
-    post: (req, res) => {
-        const {
-            userName,
-            signatureColor,
-            email,
-            password,
-            checkPassword,
-        } = req.body;
-        User.findOne({
-            where: {
-                userName: userName,
-                email: email,
-            },
-        })
-            .then((data) => {
-                if (data) {
-                    res.status(401).send('Username, Email Check');
-                    console.log('이메일, 닉네임 확인해주세요.');
-                } else if (!data) {
-                    if (password !== checkPassword) {
-                        res.status(401).send('Password Not Equle');
-                        console.log('패스워드 확인하세요.');
-                    } else if (password === checkPassword) {
-                        User.create({
-                            userName: userName,
-                            signatureColor: signatureColor,
-                            email: email,
-                            password: password,
-                        })
-                            .then((data) => {
-                                res.status(200).send(data);
-                                console.log('가입되었습니다.');
-                                //res.redirect('/user/signIn');
-                            })
-                            .catch((err) => {
-                                res.status(500).send(err);
-                                console.log('500 Error');
-                            });
-                    }
-                }
+  post: (req, res) => {
+    const { userName, email, password, checkPassword } = req.body;
+    User.findOne({
+      where: {
+        userName: userName,
+        email: email,
+      },
+    })
+      .then((data) => {
+        if (data) {
+          res.status(401).send('이메일, 닉네임을 확인해주세요.');
+        } else if (!data) {
+          if (password !== checkPassword) {
+            res.status(401).send('비밀번호와 비밀번호 재입력이 다릅니다.');
+          } else if (password === checkPassword) {
+            User.create({
+              userName,
+              signatureColor: '#f7f7f7',
+              email,
+              password,
             })
-            .catch((err) => {
+              .then((data) => {
+                res.status(200).send(data);
+              })
+              .catch((err) => {
                 res.status(500).send(err);
-                console.log('500 Error');
-            });
-    },
+              });
+          }
+        }
+      })
+      .catch((err) => {
+        res.status(500).send(err);
+      });
+  },
 };
