b51915a99cb0b17907b2b75ea7733bc8e99e0e37
Heo-JiSeong <heojiseong0811@gmail.com>
2020-07-10 00:03:49 +0900

    :wrench: Updated paletteCreate API
    - visit  기본 값을 0으로 생성하게끔 수정하였습니다.

--- server/controller/paletteController/paletteCreate.js
+++ server/controller/paletteController/paletteCreate.js
@@ -1,42 +1,43 @@
 const { Palette } = require('../../models');
 
 module.exports = {
-    post: (req, res) => {
-        const {
-            id,
-            paletteName,
-            description,
-            colorCode01,
-            colorCode02,
-            colorCode03,
-            colorCode04,
-            colorCode05,
-            colorCode06,
-            colorCode07,
-        } = req.body;
-        Palette.create({
-            userId: id,
-            paletteName,
-            description,
-            colorCode01,
-            colorCode02,
-            colorCode03,
-            colorCode04,
-            colorCode05,
-            colorCode06,
-            colorCode07,
-        })
-            .then((data) => {
-                if (data) {
-                    res.status(200).send(data);
-                    console.log('팔레트가 생성되었습니다.');
-                } else {
-                    res.status(404).send('Bad Request');
-                    console.log('404 Error');
-                }
-            })
-            .catch((err) => {
-                res.status(500).send(err);
-            });
-    },
+  post: (req, res) => {
+    const {
+      id,
+      paletteName,
+      description,
+      colorCode01,
+      colorCode02,
+      colorCode03,
+      colorCode04,
+      colorCode05,
+      colorCode06,
+      colorCode07,
+    } = req.body;
+    Palette.create({
+      userId: id,
+      paletteName,
+      description,
+      colorCode01,
+      colorCode02,
+      colorCode03,
+      colorCode04,
+      colorCode05,
+      colorCode06,
+      colorCode07,
+      visit: '0',
+    })
+      .then((data) => {
+        if (data) {
+          res.status(200).send(data);
+          console.log('팔레트가 생성되었습니다.');
+        } else {
+          res.status(404).send('팔레트 생성에 실패하였습니다.');
+          console.log('404 Error');
+        }
+      })
+      .catch((err) => {
+        res.status(500).send(err);
+      });
+  },
 };
