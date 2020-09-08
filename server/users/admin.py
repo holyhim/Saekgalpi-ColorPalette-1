09c7c188234007ccce5cf0eb65a7b8257ee47bbe
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-09-08 21:08:35 +0900

    :card_file_box: Update user DB set
    - 커스텀 유저 필드를 admin으로 이전했습니다.

--- server/users/admin.py
+++ server/users/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
 # users folder에 admin?
+
+
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    pass
