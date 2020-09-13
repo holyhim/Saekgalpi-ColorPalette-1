ed26dfe7eb080d1663e670cb7b703efc4d22dc91
holyhim <61106972+holyhim@users.noreply.github.com>
2020-09-13 17:22:34 +0900

    Pull upstream dev

--- server/palettes/models.py
+++ server/palettes/models.py
@@ -1,17 +1,16 @@
 from django.db import models
-
 from django_mysql.models import ListCharField
 
 
 class Palettes(models.Model):
-    id = models.CharField(max_length=7)
-    userId = models.CharField()
-    paletteName = models.CharField()
-    description = models.CharField()
+    id = models.CharField(max_length=7, primary_key=True)
+    userId = models.CharField(max_length=255)
+    paletteName = models.CharField(max_length=255)
+    description = models.TextField(max_length=255)
     colors = ListCharField(
         base_field=models.CharField(max_length=7), size=7, max_length=(7 * 11)
     )
-    visit = models.CharField()
+    visit = models.CharField(max_length=255)
 
 
 # Create your models here.
--- server/users/models.py
+++ server/users/models.py
@@ -2,12 +2,12 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-# 배열이 되는가?
+# 배열이 되는가? 된다 200 ok
 
 
 class User(AbstractUser):
-    id = models.CharField()
-    email = models.CharField()
-    password = models.CharField()
-    userName = models.CharField()
-    signatureColor = models.CharField()
+    user_id = models.CharField(max_length=255, primary_key=True)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=11)
+    userName = models.CharField(max_length=100)
+    signatureColor = models.CharField(max_length=7)
