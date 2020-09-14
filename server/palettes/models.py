d37ab82646c6fcdc809a59202a79628cc1045f22
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-09-14 21:18:46 +0900

    :sparkles: Create palette Validation
    - 팔레트의 커스텀 유효성 검사를 추가했습니다. (글자수 제한)

--- server/palettes/models.py
+++ server/palettes/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 from django_mysql.models import ListCharField
 from users import models as user_models
+from django.core.exceptions import ValidationError
+
+
+def validate_palette_Name(value):
+    if len(value) > 20:
+        raise ValidationError("팔레트 이름을 20 글자 이하로 설정해 주세요.")
+    else:
+        return value
+
+
+def validate_palette_Description(value):
+    if len(value) < 3:
+        raise ValidationError("팔레트 설명을 3 글자 이상으로 설정해 주세요.")
+    else:
+        return value
 
 
 class Palettes(models.Model):
@@ -9,8 +24,10 @@ class Palettes(models.Model):
 
     user_id = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
     username = models.CharField(max_length=100)
-    paletteName = models.CharField(max_length=256)
-    description = models.TextField(default="")
+    paletteName = models.CharField(max_length=256, validators=[validate_palette_Name])
+    description = models.TextField(
+        default="", validators=[validate_palette_Description]
+    )
     colors = ListCharField(
         base_field=models.CharField(max_length=10), size=7, max_length=(7 * 11)
     )
